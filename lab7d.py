5e619bf686cbd51d36d3829745585dddb680d403
vdesai19-seneca <156854849+vdesai19-seneca@users.noreply.github.com>
2024-11-18 11:24:18 -0500

    lab7d

--- lab7d.py
+++ lab7d.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: vdesai19
 class Time:
     """Simple object type for time of the day.
         data attributes: hour, minute, second
@@ -53,3 +53,4 @@ def sec_to_time(seconds):
     minutes, time.second = divmod(seconds, 60)
     time.hour, time.minute = divmod(minutes, 60)
     return time
+
