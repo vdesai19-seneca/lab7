aebc74592b3c1b559ef147f9e1785fb1d61ff605
vdesai19-seneca <156854849+vdesai19-seneca@users.noreply.github.com>
2024-11-18 11:22:23 -0500

    lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: vdesai19
+# Date: 2024-11-15
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,6 +21,15 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    if sum.second >= 60:
+        sum.minute += sum.second // 60
+        sum.second = sum.second % 60
+
+    if sum.minute >= 60:
+        sum.hour += sum.minute // 60
+        sum.minute = sum.minute % 60
+
     return sum
 
 def valid_time(t):
